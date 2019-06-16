f7bfed3c855f02f60ed5dd0d45b3109c17cf9320
ooxoop <50217839+ooxoop@users.noreply.github.com>
2019-06-16 10:23:14 +0800

    v1.2

--- zquAutoLogin.sh
+++ zquAutoLogin.sh
@@ -1,29 +1,16 @@
 #! /bin/bash
-ping -c 1 qq.com > /dev/null 2>&1
-
-if [ $? -ne 0 ];then
-	echo 检测网络连接异常
-    if [ ! -f "/tmp/zquAutoLogin-go" ];then
-        echo [zquAutoLogin] 程序不存在,且无法联网
-        exit 1
-    fi
-    chmod 777 /tmp/zquAutoLogin-go
-    /tmp/zquAutoLogin-go -u $1 -p $2
-fi
-
-echo [zquAutoLogin] 网络状态正常
-echo [zquAutoLogin] 学号:$1 密码:$2
-
 if [ ! -f "/tmp/zquAutoLogin-go" ];then
-	echo [zquAutoLogin] 程序不存在,开始下载自动登录程序...
-	## 【注意】请根据系统架构修改程序下载链接
-	wget -O /tmp/zquAutoLogin-go https://zqu-auto-login-go-1252708919.cos.ap-guangzhou.myqcloud.com/zquAutoLogin-go_linux_mipsle
-	echo [zquAutoLogin] 程序下载完成
+        echo [zquAutoLogin] 程序不存在,开始下载自动登录程序...  >> /tmp/syslog.log
+        wget -O /tmp/zquAutoLogin-go https://zqu-auto-login-go-1252708919.cos.ap-guangzhou.myqcloud.com/zquAutoLogin-go_linux_mipsle
+        if [ $? -eq 0 ];then
+                echo [zquAutoLogin] 程序下载完成,开始运行程序 >> /tmp/syslog.log
+        else 
+                echo [zquAutoLogin] 程序下载失败 >> /tmp/syslog.log
+                exit 1
+        fi
 else
-	echo [zquAutoLogin] 程序存在,开始运行程序
+        echo [zquAutoLogin] 程序存在,开始运行程序 >> /tmp/syslog.log
 fi
-    echo [zquAutoLogin] 程序运行完毕
-
 chmod 777 /tmp/zquAutoLogin-go
-/tmp/zquAutoLogin-go -u $1 -p $2
+/tmp/zquAutoLogin-go -u $1 -p $2 >> /tmp/syslog.log
  
